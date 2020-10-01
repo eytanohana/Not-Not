12a89ebd430e912c610f31db19b1253f30a13148
eytanohana <eohana98@gmail.com>
2020-10-02 02:24:00 +0300

    display text

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 import pygame
+import random
 from inputs import get_gamepad
+
 
 WIDTH = 800
 HEIGHT = 500
 
 BLUE = (12, 133, 127)
 WHITE = (255, 255, 255)
-GREY = (128, 128, 128)
+GREY = (77,) * 3
 
 RADIUS = 5
 
-moves = {'left', 'right', 'up', 'down'}
+moves = ['left', 'right', 'up', 'down']
 
 if __name__ == '__main__':
 
     pygame.init()
+
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('NOT NOT')
+
     screen.fill(BLUE)
 
-    pygame.draw.circle(screen, WHITE, (WIDTH//2, HEIGHT//2), RADIUS)
 
-    while True:
-        pygame.display.flip()
+
+    print(pygame.font.get_fonts())
+
+    font = pygame.font.SysFont('comicsansms', 60)
+    text = font.render(random.choice(moves), True, GREY)
+    text_rect = text.get_rect()
+    text_rect.center = (WIDTH // 2, HEIGHT // 2)
+
+    screen.blit(text, text_rect)
+
+    pygame.draw.circle(screen, WHITE, (WIDTH // 2, HEIGHT // 2), RADIUS)
+
+    pygame.display.flip()
+
+    while 1:
+        pass
 
 
 
