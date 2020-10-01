481aaf320e364d31b57f45ba4d362074ebde8121
eytanohana <eohana98@gmail.com>
2020-10-02 02:01:53 +0300

    display the screen

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 import pygame
+from inputs import get_gamepad
+
+WIDTH = 800
+HEIGHT = 500
+
+BLUE = (12, 133, 127)
+WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
+
+RADIUS = 5
+
+moves = {'left', 'right', 'up', 'down'}
+
+if __name__ == '__main__':
+
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen.fill(BLUE)
+
+    pygame.draw.circle(screen, WHITE, (WIDTH//2, HEIGHT//2), RADIUS)
+
+    while True:
+        pygame.display.flip()
+
+
+
+
+
