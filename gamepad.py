ebddf7708b490bf3cdf295d546c6854676e8b638
eytanohana <eohana98@gmail.com>
2020-10-05 00:53:55 +0300

    create gampad class

--- gamepad.py
+++ gamepad.py
@@ -1,22 +1,87 @@
-import pygame
 
-event_codes = {
-        'ABS_RZ': 'RT',  # right trigger: 0 - 255
-        'ABS_Z': 'LT',  # left trigger : 0 - 255
-        'BTN_TL': 'LB',  # left button  : 0 or 1
-        'BTN_TR': 'RB',  # right button : 0 or 1
-        'ABS_HAT0Y': 'D_VERT',  # D pad up down: up -1 down 1
-        'ABS_HAT0X': 'D_HORIZ',  # D pad left right: left -1 right 1
-        'BTN_EAST': 'B',  # B : 0 or 1
-        'BTN_SOUTH': 'A',  # A : 0 or 1
-        'BTN_WEST': 'X',  # X : 0 or 1
-        'BTN_NORTH': 'Y',  # Y : 0 or 1
-        'ABS_RX': 'RS_HORIZ',  # right stick left right: center 128 right 32767 left -32768
-        'ABS_RY': 'RS_VERT',  # right stick up down: center 128 up 32767 down -32768
-        'ABS_X': 'LS_HORIZ',  # left stick left right: center 128 right 32767 left -32768
-        'ABS_Y': 'LS_VERT',  # left stick up down: center 128 up 32767 down -32768
-        'BTN_THUMBR': 'RS_BTN',  # right stick button: pressed 1 unpressed 0
-        'BTN_THUMBL': 'LS_BTN',  # left stick button: pressed 1 unpressed 0
-        'BTN_SELECT': 'START',  # start button: pressed 1 unpressed 0
-        'BTN_START': 'BACK',  # back button: pressed 1 unpressed 0
-    }
+# logitech controller gamepad F310 settings
+logitech_f310_mappings = {
+    'LS_HORIZ': 0, 'LS_VERT': 1, 'TRIGGERS': 2, 'RS_VERT': 3, 'RS_HORIZ': 4,
+    'A': 0, 'B': 1, 'X': 2, 'Y': 3,
+    'LB': 4, 'RB': 5, 'BACK': 6, 'START': 7,
+    'LS_BTN': 8, 'RS_BTN': 9
+}
+
+# hats in pygame
+d_pad_values = {
+    'UP': (0, 1),
+    'DOWN': (0, -1),
+    'LEFT': (-1, 0),
+    'RIGHT': (1, 0)
+}
+
+class GamePad:
+
+    def __init__(self, gamepad, settings):
+        self.gamepad = gamepad
+        self.settings = dict(settings)
+
+    def dpad(self, i=0):
+        return self.gamepad.get_hat(i)
+
+    def is_pressed(self, btn):
+        return self.gamepad.get_button(self.settings[btn])
+
+    @property
+    def left_stick_horizontal(self):
+        return self.gamepad.get_axis(self.settings['LS_HORIZ'])
+
+    @property
+    def right_stick_horizontal(self):
+        return self.gamepad.get_axis(self.settings['RS_HORIZ'])
+
+    @property
+    def left_stick_vertical(self):
+        return self.gamepad.get_axis(self.settings['LS_VERT'])
+
+    @property
+    def right_stick_vertical(self):
+        return self.gamepad.get_axis(self.settings['RS_VERT'])
+
+    @property
+    def trigger(self):
+        return self.gamepad.get_axis(self.settings['TRIGGERS'])
+
+    # left stick 4 basic directions
+    def left_stick_left(self):
+        return self.left_stick_horizontal < -0.7
+
+    def left_stick_right(self):
+        return self.left_stick_horizontal > 0.7
+
+    def left_stick_up(self):
+        return self.left_stick_vertical < -0.7
+
+    def left_stick_down(self):
+        return self.left_stick_vertical > 0.7
+
+    # right stick 4 basic directions
+    def right_stick_left(self):
+        return self.right_stick_horizontal < -0.7
+
+    def right_stick_right(self):
+        return self.right_stick_horizontal > 0.7
+
+    def right_stick_up(self):
+        return self.right_stick_vertical < -0.7
+
+    def right_stick_down(self):
+        return self.right_stick_vertical > 0.7
+
+    def dpad_left(self, i=0):
+        return self.dpad(i) == (-1, 0)
+
+    def dpad_right(self, i=0):
+        return self.dpad(i) == (1, 0)
+
+    def dpad_up(self, i=0):
+        return self.dpad(i) == (0, 1)
+
+    def dpad_down(self, i=0):
+        return self.dpad(i) == (0, -1)
+
