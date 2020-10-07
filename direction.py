c9d0b59787fc8d98c3d5c4b9450b9148d77fc56e
eytanohana <eohana98@gmail.com>
2020-10-07 15:25:46 +0300

    add new directions

--- direction.py
+++ direction.py
@@ -1,31 +1,44 @@
 import random
 
 simple_directions = {'UP', 'DOWN', 'LEFT', 'RIGHT'}
-nots = ['', 'NOT', 'NOT NOT', 'NOT NOT NOT']
+# nots = ['', 'NOT', 'NOT NOT', 'NOT NOT NOT']
 
 class Direction:
 
-    def __init__(self, random_seed=42):
-        self.random_seed = random_seed
+    def __init__(self, difficulty=0):
+        self.difficulty = difficulty
         self.direction = None
-        random.seed(random_seed)
 
     def pick_direction(self):
-        num_nots = random.choice(nots)
-        direction = random.choice(simple_directions)
-        self.target_direction = num_nots + direction
+        self.num_nots = random.randint(0, self.difficulty)
+        nots = 'NOT ' * self.num_nots
+        direction = random.choice(list(simple_directions))
+        self.target_direction = nots + direction
 
-    def correct_direction(self, input_dir, target_dir):
-        components = target_dir.split(' ')
+    def correct_direction(self, input_dir):
+        components = self.target_direction.split(' ')
+        *prefix_nots, direction = components
 
-        *prefix_nots, dir = components
-        print(prefix_nots, dir)
+        if self.num_nots % 2 == 0:
+            return input_dir == direction
+        else:
+            return input_dir in simple_directions - {direction}
+
+    def __str__(self):
+        return f'Target Direction: {self.target_direction}'
+
 
 if __name__ == '__main__':
-    direction = Direction()
+    direction = Direction(difficulty=3)
 
+    for i in range(20):
+        direction.pick_direction()
+        print(direction.target_direction)
+        print('-' * len(direction.target_direction))
 
-    direction.correct_direction('RIGHT', 'RIGHT')
-    direction.correct_direction('RIGHT', 'NOT RIGHT')
-    direction.correct_direction('RIGHT', 'NOT NOT RIGHT')
-    direction.correct_direction('RIGHT', 'NOT NOT NOT RIGHT')
+        print('UP:', direction.correct_direction('UP'))
+        print('DOWN:', direction.correct_direction('DOWN'))
+        print('LEFT:', direction.correct_direction('LEFT'))
+        print('RIGHT:', direction.correct_direction('RIGHT'))
+
+        print()
